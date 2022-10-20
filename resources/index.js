24665d9404c308dda2c89cc19f92bed119dd8fa4
PatrickTey <93598645+PatrickTey@users.noreply.github.com>
2022-10-20 16:13:16 +0000

    first

---
+++ resources/index.js
@@ -0,0 +1,8 @@
+
+// Finds date difference
+
+let currentYear = new Date().getFullYear()
+
+let noOfYears = currentYear - 2020;
+
+document.getElementById('years').innerHTML = `${noOfYears}+ years `;
